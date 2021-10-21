9e1dd2a3caa7c05c577da8640a072c4d07b44744
Janardhana <janachet@amazon.com>
2021-10-21 12:46:53 +0530

    rendering using ejs and bootstrap

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const path = require("path");
 
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.get(
     '/',
     (req,res) => {
-        res.send(
-            {hi : 'Chetan'}
-        );
+        res.render("index");
     }
 );
 
