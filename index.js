5e23df0cf3b3652c34085a9a5b682f2c0805a858
Janardhana <janachet@amazon.com>
2021-10-21 12:55:26 +0530

    added basic functionality for menu navigation and sending data from server to views

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require("path");
 
-
+// Creating the Express server
 const app = express();
 
+// Server configuration
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "public")));
 
-
+// GET /
 app.get(
     '/',
     (req,res) => {
@@ -16,9 +17,27 @@ app.get(
     }
 );
 
+// GET /about
+app.get(
+    "/about", 
+    (req, res) => {
+        res.render("about");
+    }
+);
 
+// GET /data
+app.get(
+    "/data", 
+    (req, res) => {
+        const test = {
+            title: "Test",
+            items: ["one", "two", "three"]
+        };
+        res.render("data", { model: test });
+    }
+);
 
-
+// Starting the server
 const port = process.env.PORT || 5000;
 app.listen(
     port,
